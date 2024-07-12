c70bf7c43e4314087d0bb2cfc70720d5ff8376f4
Dishant <dyno3ddd@gmail.com>
2024-07-13 04:56:06 +0530

    Hero section done

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,44 @@
+export default function Hero() {
+    return <div className="text-center">
+
+
+
+
+        <div className="flex justify-center items-center">
+
+            <div className="">
+                <img className="w-[451px]" src="/Barsik.png" />
+            </div>
+
+
+
+
+            <div className="">
+
+                <div className="flex items-center -mb-32 font-secondary">
+
+                    <h1 className="bg-clip-text bg-gradient-to-br from-[#2AECD5] to-[#2AEC8F] text-[330px] text-transparent">10X</h1>
+
+
+
+                    <h1 className="pl-7 text-[#2AEC8F] text-[216px]">Barsik</h1>
+
+                </div>
+
+                <div className="mb-52">
+                    <p className="font-medium text-[44px]">The easiest way to <span className="text-[#2AEC8F]"> 10X </span> your Barsik</p>
+                </div>
+
+
+            </div>
+
+
+
+
+
+        </div>
+
+
+
+    </div>;
+}
